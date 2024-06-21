0021a71e93bf462713f26d411a3f9da5f119ea19
ThomasRavetto <travetto18@gmail.com>
2024-06-21 21:33:21 +0200

    adding animations to header title

--- src/app/components/home/header.tsx
+++ src/app/components/home/header.tsx
@@ -1,13 +1,25 @@
+'use client'
+
 import Image from "next/image";
 import headerImage from "../../../../public/home/headerImage - dimensioni medie.png";
+
+import { motion, Variants } from 'framer-motion';
 
 export default function Header () {
     return (
         <div id='header' className="header-container mt-20 w-screen flex flex-row justify-center items-center">
-            <div className="h-full flex flex-col justify-center items-center h-1/4">
-                <h1 className="text-4xl md:text-8xl font-semibold p-2"><span className="primary">Thomas</span> Ravetto</h1>
-                <p className="text-xl md:text-4xl font-extralight p-2">I am a <span className="primary">Software Developer</span> based in Turin</p>
-            </div>
+            <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 100, y: 0 }}
+            transition={{
+                bounce: 0,
+                duration: 1
+              }}>
+                <div className="h-full flex flex-col justify-center items-center h-1/4">
+                    <h1 className="text-4xl md:text-8xl font-semibold p-2"><span className="primary">Thomas</span> Ravetto</h1>
+                    <p className="text-xl md:text-4xl font-extralight p-2">I am a <span className="primary">Software Developer</span> based in Turin</p>
+                </div>
+            </motion.div>
             <Image
                 src={headerImage}
                 alt="Half face close-up image"
